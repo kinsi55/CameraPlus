e038d5e39c29f6914e35f530f95c913e07620f82
Kinsi <kinsi55@users.noreply.github.com>
2021-01-13 18:18:32 +0100

    Whitespace fix

--- CameraPlus/Plugin.cs
+++ CameraPlus/Plugin.cs
@@ -16,161 +16,149 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
 namespace CameraPlus {
-	[Plugin(RuntimeOptions.SingleStartInit)]
-	public class Plugin {
-		private bool _init;
-		private Harmony _harmony;
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin {
+        private bool _init;
+        private Harmony _harmony;
 
-		public Action<Scene, Scene> ActiveSceneChanged;
-		public ConcurrentDictionary<string, CameraPlusInstance> Cameras = new ConcurrentDictionary<string, CameraPlusInstance>();
+        public Action<Scene, Scene> ActiveSceneChanged;
+        public ConcurrentDictionary<string, CameraPlusInstance> Cameras = new ConcurrentDictionary<string, CameraPlusInstance>();
 
-		public static Plugin Instance { get; private set; }
-		public static string Name => "CameraPlus";
-		public static string MainCamera => "cameraplus";
+        public static Plugin Instance { get; private set; }
+        public static string Name => "CameraPlus";
+        public static string MainCamera => "cameraplus";
 
-		public RootConfig _rootConfig;
-		public ProfileChanger _profileChanger;
-		public string _currentProfile;
+        public RootConfig _rootConfig;
+        public ProfileChanger _profileChanger;
+        public string _currentProfile;
 
-		public bool MultiplayerSessionInit;
+        public bool MultiplayerSessionInit;
 
-		public Transform _origin;
-		public VRCenterAdjust vrCenterAdjust;
+        public Transform _origin;
+        public VRCenterAdjust vrCenterAdjust;
 
-		[Init]
-		public void Init(IPALogger logger) {
-			Logger.log = logger;
-			Logger.Log("Logger prepared", LogLevel.Debug);
-			string path = Path.Combine(UnityGame.UserDataPath, $"{Plugin.Name}.ini");
-			_rootConfig = new RootConfig(path);
-			if(_rootConfig.ForceDisableSmoothCamera) {
-				try {
-					string gameCfgPath = Path.Combine(Application.persistentDataPath, "settings.cfg");
-					var settings = JsonConvert.DeserializeObject<ConfigEntity>(File.ReadAllText(gameCfgPath));
-					if(settings.version == "1.6.0") {
-						if(settings.smoothCameraEnabled == 1) {
-							settings.smoothCameraEnabled = 0;
-							File.WriteAllText(gameCfgPath, JsonConvert.SerializeObject(settings));
-						}
-					}
-				} catch(Exception e) {
-					Logger.Log($"Fail SmoothCamera off {e.Message}", LogLevel.Error);
-				}
-			}
+        [Init]
+        public void Init(IPALogger logger) {
+            Logger.log = logger;
+            Logger.Log("Logger prepared", LogLevel.Debug);
+            string path = Path.Combine(UnityGame.UserDataPath, $"{Plugin.Name}.ini");
+            _rootConfig = new RootConfig(path);
+            if(_rootConfig.ForceDisableSmoothCamera) {
+                try {
+                    string gameCfgPath = Path.Combine(Application.persistentDataPath, "settings.cfg");
+                    var settings = JsonConvert.DeserializeObject<ConfigEntity>(File.ReadAllText(gameCfgPath));
+                    if(settings.version == "1.6.0") {
+                        if(settings.smoothCameraEnabled == 1) {
+                            settings.smoothCameraEnabled = 0;
+                            File.WriteAllText(gameCfgPath, JsonConvert.SerializeObject(settings));
+                        }
+                    }
+                } catch(Exception e) {
+                    Logger.Log($"Fail SmoothCamera off {e.Message}", LogLevel.Error);
+                }
+            }
 
-			//var x = AccessTools.PropertySetter(typeof(Camera), "cullingMask");
+            //var x = AccessTools.PropertySetter(typeof(Camera), "cullingMask");
 
-			//var d = x.MethodHandle;
+            //var d = x.MethodHandle;
 
-			//RuntimeHelpers.PrepareMethod(d);
+            //RuntimeHelpers.PrepareMethod(d);
 
-			//var y = d.GetFunctionPointer();
-			//maskSetterAddr = (byte*)y.ToPointer();
-			//cullingMask_ogOp = *maskSetterAddr;
+            //var y = d.GetFunctionPointer();
+            //maskSetterAddr = (byte*)y.ToPointer();
+            //cullingMask_ogOp = *maskSetterAddr;
 
-			//Console.WriteLine(">> {0} {1}", y, (int)maskSetterAddr);
-		}
+            //Console.WriteLine(">> {0} {1}", y, (int)maskSetterAddr);
+        }
 
-		[OnStart]
-		public void OnApplicationStart() {
-			if(_init) return;
-			_init = true;
-			Instance = this;
+        [OnStart]
+        public void OnApplicationStart() {
+            if(_init) return;
+            _init = true;
+            Instance = this;
 
-			_harmony = new Harmony("com.brian91292.beatsaber.cameraplus");
-			try {
-				_harmony.PatchAll(Assembly.GetExecutingAssembly());
-			} catch(Exception ex) {
-				Logger.Log($"Failed to apply harmony patches! {ex}", LogLevel.Error);
-			}
+            _harmony = new Harmony("com.brian91292.beatsaber.cameraplus");
+            try {
+                _harmony.PatchAll(Assembly.GetExecutingAssembly());
+            } catch(Exception ex) {
+                Logger.Log($"Failed to apply harmony patches! {ex}", LogLevel.Error);
+            }
 
-			SceneManager.activeSceneChanged += this.OnActiveSceneChanged;
-			// Add our default cameraplus camera
-			CameraUtilities.AddNewCamera(Plugin.MainCamera);
-			CameraProfiles.CreateMainDirectory();
+            SceneManager.activeSceneChanged += this.OnActiveSceneChanged;
+            // Add our default cameraplus camera
+            CameraUtilities.AddNewCamera(Plugin.MainCamera);
+            CameraProfiles.CreateMainDirectory();
 
-			_profileChanger = new ProfileChanger();
-			MultiplayerSessionInit = false;
-			Logger.Log($"{Plugin.Name} has started", LogLevel.Notice);
-		}
+            _profileChanger = new ProfileChanger();
+            MultiplayerSessionInit = false;
+            Logger.Log($"{Plugin.Name} has started", LogLevel.Notice);
+        }
 
-		bool isRestartingSong = false;
+        bool isRestartingSong = false;
 
-		//public bool _freezeMask = false;
-		//unsafe byte* maskSetterAddr = (byte*)0;
-		//byte cullingMask_ogOp = 0;
-		//public unsafe bool freezeMask {
-		//	get { return _freezeMask; }
-		//	set {
-		//		if(maskSetterAddr == (byte*)0) return;
+        //public bool _freezeMask = false;
+        //unsafe byte* maskSetterAddr = (byte*)0;
+        //byte cullingMask_ogOp = 0;
+        //public unsafe bool freezeMask {
+        //	get { return _freezeMask; }
+        //	set {
+        //		if(maskSetterAddr == (byte*)0) return;
 
-		//		*maskSetterAddr = value ? (byte)0xC3 : cullingMask_ogOp;
+        //		*maskSetterAddr = value ? (byte)0xC3 : cullingMask_ogOp;
 
-		//		_freezeMask = value;
-		//	}
-		//}
+        //		_freezeMask = value;
+        //	}
+        //}
 
-        public void OnActiveSceneChanged(Scene from, Scene to)
-        {
+        public void OnActiveSceneChanged(Scene from, Scene to) {
             if(isRestartingSong && to.name != "GameCore") return;
-			//if(isRestartingSong && to.name == "GameCore" && freezeMask) freezeMask = false;
+            //if(isRestartingSong && to.name == "GameCore" && freezeMask) freezeMask = false;
 
-			SharedCoroutineStarter.instance.StartCoroutine(DelayedActiveSceneChanged(from, to));
+            SharedCoroutineStarter.instance.StartCoroutine(DelayedActiveSceneChanged(from, to));
         }
 
         [HarmonyPatch(typeof(StandardLevelRestartController))]
-		[HarmonyPatch("RestartLevel")]
-		class hookRestart {
-			static void Prefix() {
-				Instance.isRestartingSong = true;
-				//Instance.freezeMask = true;
-			}
-		}
+        [HarmonyPatch("RestartLevel")]
+        class hookRestart {
+            static void Prefix() {
+                Instance.isRestartingSong = true;
+                //Instance.freezeMask = true;
+            }
+        }
 
-		private IEnumerator DelayedActiveSceneChanged(Scene from, Scene to)
-        {
+        private IEnumerator DelayedActiveSceneChanged(Scene from, Scene to) {
             bool isRestart = isRestartingSong;
             isRestartingSong = false;
 
-			CameraUtilities.ReloadCameras();
+            CameraUtilities.ReloadCameras();
 
-			if(!isRestart)
+            if(!isRestart)
                 yield return new WaitForSeconds(0.2f);
 
-            if (ActiveSceneChanged != null)
-            {
-                if (_rootConfig.ProfileSceneChange)
-                {
-                    if (to.name == "GameCore" && _rootConfig.GameProfile != "" && (!MultiplayerSession.ConnectedMultiplay || _rootConfig.MultiplayerProfile=="" ))
-                    {
+            if(ActiveSceneChanged != null) {
+                if(_rootConfig.ProfileSceneChange) {
+                    if(to.name == "GameCore" && _rootConfig.GameProfile != "" && (!MultiplayerSession.ConnectedMultiplay || _rootConfig.MultiplayerProfile == "")) {
                         _profileChanger.ProfileChange(_rootConfig.GameProfile);
-                    }
-                    else if ((to.name == "MenuCore" || to.name == "HealthWarning") && _rootConfig.MenuProfile != "" && (!MultiplayerSession.ConnectedMultiplay || _rootConfig.MultiplayerProfile == ""))
-                    {
+                    } else if((to.name == "MenuCore" || to.name == "HealthWarning") && _rootConfig.MenuProfile != "" && (!MultiplayerSession.ConnectedMultiplay || _rootConfig.MultiplayerProfile == "")) {
                         _profileChanger.ProfileChange(_rootConfig.MenuProfile);
                     }
                 }
 
-                while (Camera.main == null) yield return new WaitForSeconds(0.05f);
+                while(Camera.main == null) yield return new WaitForSeconds(0.05f);
 
-				// Invoke each activeSceneChanged event
-				foreach (var func in ActiveSceneChanged?.GetInvocationList())
-                {
-                    try
-                    {
+                // Invoke each activeSceneChanged event
+                foreach(var func in ActiveSceneChanged?.GetInvocationList()) {
+                    try {
                         func?.DynamicInvoke(from, to);
-                    }
-                    catch (Exception ex)
-                    {
+                    } catch(Exception ex) {
                         Logger.Log($"Exception while invoking ActiveSceneChanged:" +
                             $" {ex.Message}\n{ex.StackTrace}", LogLevel.Error);
                     }
                 }
             }
-            if (to.name == "GameCore" || to.name == "MenuCore" || to.name == "MenuViewControllers" || to.name == "HealthWarning")
-            {
+            if(to.name == "GameCore" || to.name == "MenuCore" || to.name == "MenuViewControllers" || to.name == "HealthWarning") {
                 GameObject gameObject = GameObject.Find("LocalPlayerGameCore/Origin");
-                if (gameObject == null)
+                if(gameObject == null)
                     _origin = null;
                 else
                     _origin = gameObject.transform;
@@ -181,8 +169,7 @@ namespace CameraPlus {
         }
 
         [OnExit]
-        public void OnApplicationQuit()
-        {
+        public void OnApplicationQuit() {
             MultiplayerSession.Close();
             _harmony.UnpatchAll("com.brian91292.beatsaber.cameraplus");
         }
@@ -192,12 +179,10 @@ namespace CameraPlus {
         public void OnSceneUnloaded(Scene scene) { }
         public void OnUpdate() { }
 
-        public void OnFixedUpdate()
-        {
+        public void OnFixedUpdate() {
             // Fix the cursor when the user resizes the main camera to be smaller than the canvas size and they hover over the black portion of the canvas
-            if (CameraPlusBehaviour.currentCursor != CameraPlusBehaviour.CursorType.None && !CameraPlusBehaviour.anyInstanceBusy &&
-                CameraPlusBehaviour.wasWithinBorder && CameraPlusBehaviour.GetTopmostInstanceAtCursorPos() == null)
-            {
+            if(CameraPlusBehaviour.currentCursor != CameraPlusBehaviour.CursorType.None && !CameraPlusBehaviour.anyInstanceBusy &&
+                CameraPlusBehaviour.wasWithinBorder && CameraPlusBehaviour.GetTopmostInstanceAtCursorPos() == null) {
                 CameraPlusBehaviour.SetCursor(CameraPlusBehaviour.CursorType.None);
                 CameraPlusBehaviour.wasWithinBorder = false;
             }
